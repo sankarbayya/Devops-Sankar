d55109e207346b5c6a5bb5e13e4aec89b3da3461
sankarbayya <56699696+sankarbayya@users.noreply.github.com>
2019-10-21 14:49:17 -0400

    Update Selenium.java

--- New Project/src/com/icici/loans/base/Selenium.java
+++ New Project/src/com/icici/loans/base/Selenium.java
@@ -1,6 +1,16 @@
 package com.icici.loans.base;
 
 public class Selenium {
+	
+	public void add(){
+
+		int a,b,d;
+
+		a=b+c;
+
+		
+
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
